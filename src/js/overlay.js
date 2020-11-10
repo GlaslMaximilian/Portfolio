eb639ed3d102e568de9b7bd1d44d7eb82f5c0b20
Glasl Maximilian <glasl-maxe@outlook.com>
2020-11-10 08:49:34 +0100

    Fixed Jittering
    Fixed jittering of Parallax effect by removing it

--- src/js/overlay.js
+++
@@ -1,8 +0,0 @@
-function on() {
-    document.getElementById("overlay").style.display = "block";
-    console.log('fix')
-}
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  } 
